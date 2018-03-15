3c390faddf77076dc0bb696a71e61788bc9d379c
jack andrews <jandrews@kx.com>
2018-03-15 23:29:12 +1100

    J args to as. make view

--- cc.c
+++ cc.c
@@ -6,17 +6,17 @@ enum{ax,cx, dx, bx, sp, bp, si, di,
 enum{o,u,z,g,s,b,y,x,       //o(accum) s(stack) i,j(scratch) x,y,z,u,v,w(parms) b,c,d,e,f,g(save state).   s,b,c,d no RM mode
      v,w,i,j,c,d,e,f};    
 enum{ret,mov,add,sub,imul,cqo,idiv,nop};
-G swap(G*x,G*y){G t=*x;*x=*y;*y=t;R t;}    // store load  immediate
-I as(G*b,G o,G x,G y,G m,J z) //m is 1:RR, 2:MR, 4:RM, 8:RI.  return inst length
+J swap(J*x,J*y){J t=*x;*x=*y;*y=t;R t;}    // store load  immediate
+J as(G*b,J o,J x,J y,J m) //m is 1:RR, 2:MR, 4:RM, 8:RI.  return inst length
 {G*a=b;
  G rex(G w){R 0x48|w*8|(x>7)*4|y>7;}
- G modrm(){R (0||m|6)*0xC0|(x&7)*8|y&7;}
+ G modrm(){R (0||m&1)*0xC0|(x&7)*8|y&7;}
  G*dir(G p){if(m&2)swap(&x,&y),p+=2;*a++=rex(0),*a++=p,*a++=modrm();}
  switch(o){
  C(ret,*a++=0xC3)
  C(add,dir(0x01))
  C(sub,dir(0x29))
- C(mov,(m&8?*a++=0x48+(x>7),*a++=0xb8+x%8,*(J*)a=z,a+=8  //immediate, 
+ C(mov,(m&8?*a++=0x48+(x>7),*a++=0xb8+x%8,*(J*)a=y,a+=8  //immediate, 
            :dir(0x89)))
  C(cqo, (*a++=0x48,*a++=0x99))
  C(idiv,(y=7,*a++=rex(1),*a++=0xF7,*a++=modrm()))
@@ -26,10 +26,16 @@ I as(G*b,G o,G x,G y,G m,J z) //m is 1:RR, 2:MR, 4:RM, 8:RI.  return inst length
  R a-b;
 }
 #include<stdio.h>
+#define O printf
 int main()
 {C b[128]={0};
  FILE*f=fopen("o","w");
- I i=as(b,ret,0,0,0,0);
+ I i=0;
+ i+=as(b+i,mov,ax,cx,1);
+ i+=as(b+i,mov,ax,cx,2);
+ i+=as(b+i,mov,ax,cx,4);
+ i+=as(b+i,mov,ax,0xdeadbeef,8);
+ i+=as(b+i,ret,0,0,0);
  fwrite(b,i,1,f);
  fclose(f);
 }
--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 all:
 	gcc -Os cc.c -o cc
+view:
+	./cc
+	objdump -D -Mintel,x86-64 -b binary -m i386 o
